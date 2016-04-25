3146f804507a7ae83267eaa363542fe5428e3998
bubkoo <bubkoo.wy@gmail.com>
2016-04-25 17:38:40 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ var clamp       = require('clamp');
 var assign      = require('object-assign');
 var randomFloat = require('random-floating');
 
+
+var MAX = 180;
+var MIN = -180;
+
 module.exports = function (options) {
 
   options = assign({
     fixed: 5,
-    min: -180,
-    max: 180
+    min: MIN,
+    max: MAX
   }, options);
 
-  options.min = clamp(options.min, -180, 180);
-  options.max = clamp(options.max, -180, 180);
+  options.min = clamp(options.min, MIN, MAX);
+  options.max = clamp(options.max, MIN, MAX);
+
+  options.inspected = true;
 
   return randomFloat(options);
 };
-
